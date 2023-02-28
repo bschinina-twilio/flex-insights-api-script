6fefb0e9983bba4fb635a9feb5b6ad3b8ff0e39a
bschinina-twilio <67924770+bschinina-twilio@users.noreply.github.com>
2023-02-27 19:45:15 -0800

    Update function-get-report.js

--- function-get-report.js
+++ function-get-report.js
@@ -1,193 +1,233 @@
 /*
 
 Required Environment Variables:
-ANALYTICS_LOGIN_EMAIL
-ANALYTICS_LOGIN_PASSWORD
 ANALYTICS_WORKSPACE
 
 POST request must include reportId. Example JSON:
 {
-	"reportId":"111111"
+	"reportId":"111111",
+	"username":"your_email_address",
+	"password":"your_configured_password"
 }
+
+Contact Twilio Support if you do not have credentials.
 
 */
 
+const axios = require('axios');
 
 exports.handler = async function (context, event, callback) {
-	const axios = require('axios');
+	const response = new Twilio.Response()
 
-	let tmpToken,
-		reportResponse,
-		userLogin,
-		reportCSV
+	const workspaceId = context.ANALYTICS_WORKSPACE
+	const { reportId, username, password } = event
 
-	let { reportId } = event
+	try {
+		// get sstoken with user credentials 
+		let apiAuth = await getSuperSecuredToken(username, password)
 
-	// set up user login info
-	const data = JSON.stringify({
+		if (apiAuth) {
+			// get temporary token with sstoken
+			let tmpToken = await getTempToken(apiAuth)
+
+			if (tmpToken) {
+				// get report export
+				let reportResponse = await exportReport(tmpToken, workspaceId, reportId)
+
+				if (reportResponse) {
+					// get report CSV
+					let reportCSV = await downloadReportCsv(tmpToken, reportResponse)
+
+					// check if the CSV has data
+					if (reportCSV && typeof reportCSV.data === "string") {
+
+						//we have CSV and it is full
+						console.log(`report data: ${reportCSV.data}`);
+						return callback(null, { data: reportCSV.data });
+					} 
+					else {
+						//we have CSV, but it is empty
+						console.log("reports data is empty", reportCSV, typeof reportCSV)
+						return callback(null, { message: "no data to process" })
+
+					}
+				}
+				else {
+					let error = `ERROR: failed to get report export for ${reportId}`
+					response.setStatusCode(500)
+					response.setBody(error)
+					return callback(null, response)
+				}
+			}
+			else {
+				let error = "ERROR: failed to get temporary token"
+				response.setStatusCode(500)
+				response.setBody(error)
+				return callback(null, response)
+			}
+		}
+		else {
+			let error = "ERROR: failed to get api authentication with provided user credentials"
+			response.setStatusCode(500)
+			response.setBody(error)
+			return callback(null, response)
+		}
+	}
+	catch (e) {
+		response.setStatusCode(500)
+		return callback(null, response)
+	}
+
+}
+
+
+const getSuperSecuredToken = async (username, password) => {
+
+	// set up api authentication
+	let loginData = JSON.stringify({
 		postUserLogin: {
-			login: context.ANALYTICS_LOGIN_EMAIL,
-			password: context.ANALYTICS_LOGIN_PASSWORD,
+			login: username,
+			password: password,
 			remember: 0,
 			verify_level: 2
 		}
 	});
 
 	//format request for flex insights api "login"
-	const config = {
+	const loginConfig = {
 		method: 'post',
 		url: 'https://analytics.ytica.com/gdc/account/login',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		data: data
+		data: loginData
 	};
 
-	// API REQUEST: execute login request
+	// get secure token from the api
 	try {
-		userLogin = await axios(config);
-		console.log("got user login");
+		let apiAuth = await axios(loginConfig);
+		console.log("got user login / api auth");
+
+		return apiAuth
 	}
 	catch (e) {
-		console.log(`error logging in: ${e}`);
-		return callback(null, e);
+		console.error(`unable to get secured token with provided credentials: ${e}`)
+	}
+}
+
+
+const getTempToken = async (apiAuth) => {
+	let tmpToken
+
+	//format request for temp token
+	const tokenConfig = {
+		method: 'get',
+		url: 'https://analytics.ytica.com/gdc/account/token',
+		headers: {
+			'X-GDC-AuthSST': `${apiAuth.data.userLogin.token}`,
+			'Content-Type': 'application/json'
+		}
+	};
+
+	try {
+		// get the temp token from the api
+		tmpToken = await axios(tokenConfig);
+		console.log("got temp token");
+
+		return tmpToken.data.userToken.token
+	}
+	catch (e) {
+		console.error(`unable to get temporary token: ${e}`)
+	}
+}
+
+const exportReport = async (tmpToken, workspace_id, object_id) => {
+	let reportResponse
+
+	//set up report data
+	let reportData = JSON.stringify({
+		report_req: {
+			report: `/gdc/md/${workspace_id}/obj/${object_id}`
+		}
+	});
+
+	//format report request
+	const getReportConfig = {
+		method: 'post',
+		url: `https://analytics.ytica.com/gdc/app/projects/${workspace_id}/execute/raw`,
+		headers: {
+			Cookie: `GDCAuthTT=${tmpToken}`,
+			'Content-Type': 'application/json'
+		},
+		data: reportData
+	};
+
+	try {
+		// get report export from the api
+		reportResponse = await axios(getReportConfig);
+		console.log("got report");
+
+		return reportResponse
+	}
+	catch (e) {
+		console.error(`unable to get report export: ${e}`)
+	}
+}
+
+const downloadReportCsv = async (tmpToken, reportResponse) => {
+	let reportCSV
+
+	//format request for csv download
+	let downloadConfig = {
+		method: 'get',
+		url: `https://analytics.ytica.com${reportResponse.data.uri}`,
+		headers: {
+			Cookie: `GDCAuthTT=${tmpToken}`
+		}
 	}
 
-	// if we have the right login info
-	if (userLogin.data && userLogin.data.userLogin && userLogin.data.userLogin.token) {
-		//format request for temp token
-		const tokenconfig = {
-			method: 'get',
-			url: 'https://analytics.ytica.com/gdc/account/token',
-			headers: {
-				'X-GDC-AuthSST': `${userLogin.data.userLogin.token}`,
-				'Content-Type': 'application/json'
-			}
-		};
+	try {
+		// get report csv from the api
+		reportCSV = await axios(downloadConfig);
 
-		//API REQUEST: get temp token
-		try {
-			tmpToken = await axios(tokenconfig);
-			console.log("got temp token");
-		}
-		catch (e) {
-			console.log(`error getting temp token: ${e}`);
-			return callback(null, e);
-		}
+		// if we get a 202, try again 
+		// see docs for more details about this nuance: https://www.twilio.com/docs/flex/developer/insights/api/export-data#download-the-report
+		if (reportCSV.status === 202) {
+			console.log("csv download was not ready. trying again.")
+			reportCSV = await axios(downloadConfig)
 
-		// if we have the temp token
-		if (tmpToken.data && tmpToken.data.userToken && tmpToken.data.userToken.token) {
+			// if we got another 202, try again
+			if (reportCSV.status === 202) {
+				console.log("csv download was not ready. trying again.")
+				reportCSV = await axios(downloadConfig)
 
-			//set up config for report
-			const data = JSON.stringify({
-				report_req: {
-					report: `/gdc/md/${context.ANALYTICS_WORKSPACE}/obj/${reportId}`
+				// if it wasn't successful by now, stop trying (this can be modified to try a few more times before giving up)
+				if (reportCSV.status != 200) {
+					console.log(`unable to fetch ${reportId} after 2 tries. try again later.`)
 				}
-			});
-
-			//format report request
-			const getReportUrlConfig = {
-				method: 'post',
-				url: `https://analytics.ytica.com/gdc/app/projects/${context.ANALYTICS_WORKSPACE}/execute/raw`,
-				headers: {
-					Cookie: `GDCAuthTT=${tmpToken.data.userToken.token}`,
-					'Content-Type': 'application/json'
-				},
-				data: data
-			};
-
-			//API REQUEST: get report object
-			try {
-				reportResponse = await axios(getReportUrlConfig);
-				console.log("got report");
-			}
-			catch (e) {
-				console.log(`error getting report object: ${e}`);
-				return callback(null, e);
-			}
-
-			//if we have the report object
-			if (reportResponse && reportResponse.data && reportResponse.data.uri) {
-				//format request for csv download
-				var downloadConfig = {
-					method: 'get',
-					url: `https://analytics.ytica.com${reportResponse.data.uri}`,
-					headers: {
-						Cookie: `GDCAuthTT=${tmpToken.data.userToken.token}`
-					}
-				};
-
-				//API REQUEST: get csv download
-				try {
-					reportCSV = await axios(downloadConfig);
-
-					// if we get a 202, try again 
-					// see docs for more details about this nuance: https://www.twilio.com/docs/flex/developer/insights/api/export-data#download-the-report
-					if (reportCSV.status === 202) {
-						console.log("csv download was not ready. trying again.")
-						reportCSV = await axios(downloadConfig)
-
-						// if we got another 202, try again
-						if (reportCSV.status === 202) {
-							console.log("csv download was not ready. trying again.")
-							reportCSV = await axios(downloadConfig)
-
-							// if it wasn't successful by now, stop trying (this can be modified to try a few more times before giving up)
-							if (reportCSV.status != 200) {
-								console.log(`unable to fetch ${reportId}. try again later.`)
-							}
-							else {
-								console.log("got csv")
-							}
-						}
-						else if (reportCSV.status === 200) {
-							console.log("got csv");
-						}
-						// if we don't get a 200 or a 201, it should error in the catch block
-						// this is just in case we get something very much unexpected
-						else {
-							console.log(`something unexpected happened when downloading CSV data for ${reportId}`)
-						}
-					}
-					else if (reportCSV.status === 200) {
-						console.log("got csv");
-					}
-					else {
-						console.log(`something unexpected happened when downloading CSV data for ${reportId}`)
-					}
-				}
-				catch (e) {
-					console.log(`error getting report csv download: ${e}`);
-					return callback(null, e);
-				}
-
-				// if we have the csv download
-				if (reportCSV && typeof reportCSV.data === "string") {
-					//we have report and it is full
-					console.log(`report data: ${reportCSV.data}`);
-
-					return callback(null, { data: reportCSV.data });
-
-				} else {
-					//we got the report, but it is empty
-					console.log("reports data is empty", reportCSV, typeof reportCSV)
-					return callback(null, { message: "no data to process" })
-
+				else {
+					console.log("got csv")
 				}
 			}
+			else if (reportCSV.status === 200) {
+				console.log("got csv");
+			}
+			// if we don't get a 200 or a 201, it should error in the catch block
+			// this is just in case we get something very much unexpected
 			else {
-				console.log(`error report response: ${e}`);
-				return callback(null, e);
+				console.log(`something unexpected happened when downloading CSV data for ${reportId}`)
 			}
+		}
+		else if (reportCSV.status === 200) {
+			console.log("got csv");
 		}
 		else {
-			console.log(`error temp token: ${e}`);
-			return callback(null, e);
+			console.log(`something unexpected happened when downloading CSV data for ${reportId}`)
 		}
 
+		return reportCSV
 	}
-	else {
-		console.log(`error login data not right: ${e}`);
-		return callback(null, e);
+	catch (e) {
+		console.error(`unable to get report csv download: ${e}`)
 	}
 }
